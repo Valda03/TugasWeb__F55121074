824f669a8b797ee84b82eaf35efdf99ac6d39ce5
Valda03 <113921852+Valda03@users.noreply.github.com>
2022-10-19 01:41:26 -0800

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,17 @@
+<?php
+echo $_GET['firstname'];
+echo "<br />";
+echo $_GET['lastname'];
+echo "<br />";
+echo $_GET['alamat'];
+echo "<br />";
+echo $_GET['tanggal'];
+echo "<br />";
+echo $_GET['JenisKel'];
+echo "<br />";
+echo $_GET['username'];
+echo "<br />";
+echo $_GET['password'];
+echo "<br />";
+echo $_GET['gambar'];
+?>
